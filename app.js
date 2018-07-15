653aa3697c6bd51fd08d396c180c5f7b4b55cab2
zyq <wingkong1978@163.com>
2018-07-15 17:05:48 +0800

    test

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('我系宝宝猪！！！！');
+  let rnd = Math.floor(Math.random()*10+1);
+  if(rnd%2===1){
+    res.send('我系宝宝猪！！！！');
+  }
+  else{
+    res.send('我系猪头仔！！！！');
+  }
 });
 
 var server = app.listen(3000, function () {
